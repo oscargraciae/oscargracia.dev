b54df1b3aac1c73e06d64f0495cd4b013a6530f5
Oscar Gracia <oscar.graciae@gmail.com>
2023-12-16 15:30:50 -0500

    feat: Add Inter font

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 115,
+ };
---
+++ tailwind.config.mjs
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          '50': '#f0fdf5',
+          '100': '#dcfce8',
+          '200': '#bbf7d1',
+          '300': '#86efad',
+          '400': '#4ade80',
+          '500': '#22c55e',
+          '600': '#16a34a',
+          '700': '#15803c',
+          '800': '#166533',
+          '900': '#14532b',
+          '950': '#052e14',
+        },
+      }
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#22c55e',    // Changed to your brand's primary color
+          secondary: '#22c55e',  // Changed to your brand's secondary color
+          accent: '#22c55e',     // Changed to your brand's accent color
+          neutral: '#333333',    // Remains the same
+          'base-100': '#FFFFFF', // Remains the same
+          info: '#22c55e',       // Remains the same
+          success: '#00fabe',    // Remains the same
+          warning: '#eab308',    // Remains the same
+          error: '#ef4444'       // Remains the same
+        }
+      }
+    ]
+  },
+	plugins: [require("daisyui")],
+}
