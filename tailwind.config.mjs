9c87a3982f29091acaed593ac5beca423bc5f6c5
Oscar Gracia <oscar.graciae@gmail.com>
2024-01-20 12:32:33 -0500

    Remove unnecessary import statement in tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
